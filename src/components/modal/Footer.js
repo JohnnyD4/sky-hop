19ee361131bd62cbcac45935f4162ad1acd6da5c
John Davis <johnsdavis95@gmail.com>
2023-12-12 23:55:56 -0500

    Added Footer content and logic

--- src/components/modal/Footer.js
+++ src/components/modal/Footer.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { ModalFooter, Button } from '@chakra-ui/react';
+import { ModalFooter, Button, Flex, Text } from '@chakra-ui/react';
 
-function Footer({ toggleModal }) {
+function Footer({ toggleModal, validImport }) {
   return (
-			<ModalFooter>
+			<ModalFooter flexDirection={'column'}>
+        {validImport && (
+          <Text
+            fontWeight={'700'}
+            fontSize={'18px'}
+            marginBottom={'20px'}
+          >
+            Data in the import file is correct. Please press continue to import.
+          </Text>
+        )}
+        <Flex gap={'10px'} justifyContent={'center'} marginBottom={'40px'}>
+          <Button type={'submit'} w={'200px'} colorScheme={'primary'}>Continue Import</Button>
+          <Button w={'200px'} onClick={toggleModal} colorScheme={'secondary'} variant={'outline'}>Cancel</Button>
+        </Flex>
 			</ModalFooter>
   );
 }
--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { ModalContent } from '@chakra-ui/react';
 import Footer from './Footer';
 import Body from './Body';
 import Header from './Header';
 
 function Modal({ toggleModal }) {
+  const { handleSubmit, register } = useForm();
+	const [validImport, setValidImport] = useState(false);
+
+	const onSubmit = values => {
+		console.log(values);
+		alert('Successfully imported form.');
+		toggleModal();
+	};
+
   return (
 		<ModalContent borderRadius={'20px'}>
 			<Header toggleModal={toggleModal} />
-			<Body />
-			<Footer />
+      <form onSubmit={handleSubmit(onSubmit)}>
+				<Body register={register} setValidImport={setValidImport} />
+				<Footer toggleModal={toggleModal} validImport={validImport} />
+			</form>
 		</ModalContent>
   );
 };
